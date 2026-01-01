8d3b4d67663f833ba4000be5cc1150153bb96f9d
ahmad nawaz <ahmadnawaz.codes@gmail.com>
2025-12-31 22:17:56 -0800

    fix sidebare issue

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Sidebar from '../Sidebar/Sidebar'
+import Sidebar from '../SideBar/Sidebar'
 import './style.css'
 
 const Layout = () => {
